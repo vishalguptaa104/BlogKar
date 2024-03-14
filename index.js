57f2c588c497df83ccd4117989444018a9eb9e01
vishalguptaa104 <119746913+vishalguptaa104@users.noreply.github.com>
2024-03-14 07:29:56 +0000

    Setting up project

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const path = require("path")
+const mongoose = require('mongoose')
+
+const userRoute = require('./routes/user')
+
+const app = express();
+const PORT = 3000
+
+mongoose.connect("mongodb://localhost:27017/blogkar").then((e)=> console.log("MongoDB Connected"));
+
+app.set("view engine", 'ejs')
+app.set("views", path.resolve('./views'))
+
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req,res)=> {
+    res.render("home")
+})
+
+app.use('/user', userRoute)
+
+app.listen(PORT, ()=> {
+    console.log("running on port 3000");
+})
