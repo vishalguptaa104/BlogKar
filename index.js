994273de4e06c7cd193c7c2fb063094520b347ea
vishalguptaa104 <119746913+vishalguptaa104@users.noreply.github.com>
2024-03-17 06:56:59 +0000

    Add file storage using multer

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const path = require("path")
 const mongoose = require('mongoose')
-
 const cookieParser = require('cookie-parser')
 
+const Blog = require('./models/blog')
+
 const userRoute = require('./routes/user')
+const blogRoute = require('./routes/blog')
+
 const { checkForAuthenticationCookie } = require("./middlewares/authentication")
 
 const app = express();
@@ -18,14 +21,18 @@ app.set("views", path.resolve('./views'))
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
 app.use(checkForAuthenticationCookie("token"))
+app.use(express.static(path.resolve("./public")))
 
-app.get('/', (req,res)=> {
+app.get('/', async (req,res)=> {
+    const allBlogs = await Blog.find({});
     res.render("home", {
         user : req.user,
+        blogs: allBlogs,
     })
 })
 
 app.use('/user', userRoute)
+app.use('/blog', blogRoute)
 
 app.listen(PORT, ()=> {
     console.log("running on port 3000");
---
+++ models/blog.js
@@ -0,0 +1,29 @@
+const { Schema, default: mongoose, model } = require("mongoose");
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    //   required: true,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Blog = model("blog", blogSchema);
+
+module.exports = Blog;
